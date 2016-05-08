6590ec9b9cde15eb684b6a9553d3d90ae9378a61
nassir <nassirmz@gmail.com>
2016-05-08 15:20:27 -0700

    (feat) Implement PUT and DELETE helper functions

--- server/utils/helper.js
+++ server/utils/helper.js
@@ -11,8 +11,8 @@ module.exports = {
   createTodo: function (req, res) {
     db.todo
       .create(req.body)
-      .then(function (data) {
-        res.status(201).send(data);
+      .then(function (result) {
+        res.status(201).send(result);
       })
       .catch(function (err) {
         res.status(400).send(err.message);
@@ -21,8 +21,8 @@ module.exports = {
   createUser: function (req, res) {
     db.user
       .create(req.body)
-      .then(function (user) {
-        res.status(201).send(user);
+      .then(function (result) {
+        res.status(201).send(result);
       })
       .catch(function (err) {
         res.status(400).send(err.message);
@@ -32,11 +32,51 @@ module.exports = {
     var userID = Number(req.params.id);
     db.user
       .findById(userID)
-      .then(function (user) {
-        res.send(user);
+      .then(function (result) {
+        res.send(result);
       })
       .catch(function (err) {
         res.status(401).send(err.message);
+      });
+  },
+  updateTodo: function (req, res) {
+    var userID = Number(req.params.id);
+    db.todo
+      .findById(userID)
+      .then(function (result) {
+        result
+          .update(req.body)
+          .then(function (result) {
+            res.send(result);
+          });
+      })
+      .catch(function (err) {
+        res.status(400).send(err.message);
+      });
+  },
+  getTodo: function (req, res) {
+    var todoID = Number(req.params.id);
+    db.todo
+      .findById(todoID)
+      .then(function (result) {
+        res.send(result);
+      })
+      .catch(function (err) {
+        res.status(500).send(err.message);
+      });
+  },
+  deleteTodo: function (req, res) {
+    var todoID = Number(req.params.id);
+    db.todo
+      .findById(todoID)
+      .then(function (result) {
+        result.destroy()
+        .then(function (result) {
+          res.send(result);
+        });
+      })
+      .catch(function (err) {
+        res.status(500).send(err.message);
       });
   }
 };
--- server/utils/routes.js
+++ server/utils/routes.js
@@ -1,11 +1,18 @@
 var helpers = require('./helper');
 
 module.exports = function (app) {
+
   app.route('/todos')
     .get(helpers.getAllTodos)
     .post(helpers.createTodo);
   app.route('/users')
     .post(helpers.createUser);
+
   app.route('/users/:id')
     .get(helpers.getUser);
+
+  app.route('/todos/:id')
+    .put(helpers.updateTodo)
+    .get(helpers.getTodo)
+    .delete(helpers.deleteTodo);
 };
