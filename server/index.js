1829b7b969d271c99ab8e13c7bdc2f765045d8e5
nassir <nassirmz@gmail.com>
2016-05-08 09:44:23 -0700

    (refactor) Move db connection to index.js

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var db = require('./db/db');
 
 var app = express();
 
@@ -8,6 +9,9 @@ app.use(express.static(__dirname + '/../client'));
 
 var port = process.env.PORT || 4568;
 
-app.listen(port, function () {
-  console.log('Listening on ', port);
+db.sequelize.sync({force: true})
+.then(function () {
+  app.listen(port, function () {
+    console.log('Listening on port ', port);
+  });
 });
