93f0ce10d515236203d86b1c971592d48900868e
nassir <nassirmz@gmail.com>
2016-05-09 22:18:38 -0700

    (feat) Add backbone model controller and view files

--- client/models/todoModel.js
+++ client/models/todoModel.js
@@ -1,1 +1,6 @@
-var TodoItem = Backbone.Model.extend({});
+var TodoItem = Backbone.Model.extend({
+  urlRoot: '/todos'
+});
+
+var todoItem = new TodoItem({id: 1});
+todoItem.fetch();
--- client/views/todoView.js
+++ client/views/todoView.js
@@ -1,2 +1,11 @@
-var TodoView = Backbone.View.extend({});
-var todoModel = new TodoItem();
+var TodoView = Backbone.View.extend({
+  render: function () {
+    console.log(this.model);
+    $(this.el).html('<li>' + this.model.get('task') + '</li>');
+  }
+});
+var  todoView = new TodoView({
+  model: todoItem
+});
+
+todoView.render();
