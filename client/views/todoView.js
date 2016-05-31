d137da581502ddcd7fa3dac7e5454c5a27231c7d
nassir <nassirmz@gmail.com>
2016-05-31 10:09:10 -0700

    (feat) Update task after gets edited

--- client/views/todoView.js
+++ client/views/todoView.js
@@ -1,12 +1,13 @@
 var TodoView = Backbone.View.extend({
   initialize: function () {
-    this.model.on('change input:checkbox', this.render, this);
+    this.model.on('change:task', this.render, this);
     this.model.on('destroy', this.removeItem, this);
   },
   events: {
     'change input:checkbox': 'changeCompleted',
     'click .delete': 'deleteTask',
     'click .edit': 'changeToEditMode',
+    'click .save': 'editTask'
   },
   tagName: 'li',
   changeCompleted: function () {
@@ -20,8 +21,16 @@ var TodoView = Backbone.View.extend({
   },
   changeToEditMode: function () {
     this.$el.addClass('editMode');
-    this.$el.find('.edit').addClass('save').text('save');
+    this.$el.find('.edit').addClass('save').removeClass('edit').text('save');
     this.$el.find(':text').val(this.model.get('task'));
+  },
+  editTask: function () {
+    var newValue = this.$el.find(':text').val();
+    console.log(newValue);
+    this.model.set({ task: newValue});
+    this.$el.removeClass('editMode');
+    this.$el.find('.edit').removeClass('save').addClass('.edit').text('edit');
+    this.model.save();
   },
   template: _.template('<input type="checkbox" <% if(completed) print("checked") %>>' + '<label><%= task %></label>' + '<input type="text"><button class="edit">Edit</button><button class="delete">Delete</button'),
   render: function () {
