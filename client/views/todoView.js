4e03faf93a1811fc55c72cb5fe71385b0ee19294
nassir <nassirmz@gmail.com>
2016-06-01 22:47:37 -0700

    (fix) Render on edited todos in place

--- client/views/todoView.js
+++ client/views/todoView.js
@@ -1,7 +1,8 @@
 var TodoView = Backbone.View.extend({
   initialize: function () {
-    this.model.on('change', this.render, this);
+    this.model.on('change:completed', this.render, this);
     this.model.on('destroy', this.removeItem, this);
+    this.model.on('change:task', this.newTask, this);
   },
   events: {
     'change input:checkbox': 'changeCompleted',
@@ -32,18 +33,19 @@ var TodoView = Backbone.View.extend({
     this.$el.find('.edit').removeClass('save').addClass('.edit').text('edit');
     this.model.save();
   },
+  newTask: function () {
+    var attr = this.model.toJSON();
+    this.$el.html(this.template(attr));
+  },
   template: _.template('<input type="checkbox" <% if(completed) print("checked") %>>' + '<label><%= task %></label>' + '<input type="text"><button class="edit">Edit</button><button class="delete">Delete</button'),
   render: function () {
     var attr = this.model.toJSON();
     this.$el.html(this.template(attr));
-    // if(attr.completed) {
-    //   $('#completed-tasks').append(this.el);
-    // }
-    // else {
-    //   $('#incompleted-tasks').append(this.el);
-    // }
+    if(attr.completed) {
+      $('#completed-tasks').append(this.el);
+    }
+    else {
+      $('#incompleted-tasks').append(this.el);
+    }
   }
 });
-
-
-
