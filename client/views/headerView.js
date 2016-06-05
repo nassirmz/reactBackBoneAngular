7d6ad517202a13899a2dc2d6fd5936e853c014d5
nassir <nassirmz@gmail.com>
2016-06-04 23:59:43 -0700

    (refactor) Refactor Header elements into their own views

---
+++ client/views/headerView.js
@@ -0,0 +1,13 @@
+var HeaderView = Backbone.View.extend({
+  tagName: 'ul',
+  className: 'nav',
+  template: _.template('<li><a href="/signup">Create Account</a></li>' +
+    '<li><a href="signin">Sign In</a></li>' +
+    '<li><a href="signout">Sign Out</a></li>'),
+  render: function () {
+    this.$el.html(this.template());
+    $('.header').append(this.$el);
+  }
+});
+var headerView = new HeaderView();
+headerView.render();
