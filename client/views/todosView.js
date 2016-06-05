170174edf449d66e4425a2a3a0dbd95acaaff390
nassir <nassirmz@gmail.com>
2016-06-04 23:48:31 -0700

    (refactor) Put index.html elements into todosview template

--- client/views/todosView.js
+++ client/views/todosView.js
@@ -1,8 +1,13 @@
 var TodosView = Backbone.View.extend({
+  template: _.template(
+    '<h3>Todo</h3><ul id="incompleted-tasks"></ul>' +
+    '<h3>Completed</h3><ul id="completed-tasks"></ul>'),
   initialize: function () {
     this.collection.on('add', this.renderTodos, this);
   },
   render: function () {
+    this.$el.html(this.template());
+    $('.container').append(this.$el);
     this.collection.forEach(this.renderTodos, this);
   },
   renderTodos: function (todoItem) {
