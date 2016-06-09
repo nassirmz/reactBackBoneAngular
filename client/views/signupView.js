d52c2aa05c04c70fb7e9d35ae6d224a3ff85bba3
nassir <nassirmz@gmail.com>
2016-06-09 13:17:23 -0700

    (feat) encloste inputes in li

--- client/views/signupView.js
+++ client/views/signupView.js
@@ -1,6 +1,6 @@
-var SignupView = Backbone.Model.extend({
+var SignupView = Backbone.View.extend({
   tagName: 'form',
-  template: _.template('<input placeholder="username" type="text"><input placeholder="password" type="text"><button>Create Free Account</button>'),
+  template: _.template('<ul><li><input placeholder="username" type="text"></li><li><input placeholder="password" type="text"></li><li><button>Create Free Account</button></li></ul>'),
   render: function () {
     this.$el.html(this.template());
     $('.container').append(this.$el);
