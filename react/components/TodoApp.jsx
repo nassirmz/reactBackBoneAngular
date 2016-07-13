d35f9b40af8644a5969dcd3323319a0e337a5b94
nassir <nassirmz@gmail.com>
2016-07-13 12:20:55 -0700

    (feat) Handle fetching and updating todos from server

--- react/components/TodoApp.jsx
+++ react/components/TodoApp.jsx
@@ -1,5 +1,6 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var axios = require('axios');
 
 var AddTodo = require('AddTodo');
 var Todos = require('Todos');
@@ -7,35 +8,34 @@ var Todos = require('Todos');
 var TodoApp = React.createClass({
   getInitialState () {
     return {
-      todos: [
-        {
-          id: 1,
-          task: 'Walk 30 min',
-          completed: false
-        },
-        {
-          id: 2,
-          task: 'Workout',
-          completed: true
-        },
-        {
-          id: 3,
-          task: 'Buy TV',
-          completed: false
-        }
-      ]
+      todos: []
     };
   },
-  handleAddTodo (task) {
-    var todos = this.state.todos;
-    var todo = {
-      id: todos.length + 1,
-      task,
-      completed: false
-    }
-    this.setState ({
-      todos: [...this.state.todos, todo]
+  componentDidMount () {
+    axios.get('/todos', { headers: { 'Auth': localStorage.getItem('Auth')}})
+    .then((resp) => {
+      this.setState({
+        todos: resp.data
+      });
     });
+  },
+  handleAddTodo (task) {
+    var {todos} = this.state;
+    var todo = {
+      task
+    };
+    axios.post('/todos', todo, { headers: {'Auth': localStorage.getItem('Auth')}})
+    .then((resp) => {
+      this.setState({
+        todos: [
+          ...todos,
+          resp.data
+        ]
+      })
+    })
+    .catch((error) => {
+      console.log(error);
+    })
   },
   handleToggle (id) {
     var {todos} = this.state;
