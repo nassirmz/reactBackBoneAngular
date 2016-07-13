56e4c2f9b8916f771fec4e9439e884a5617c77ce
nassir <nassirmz@gmail.com>
2016-07-12 18:10:04 -0700

    (feat) Conditionaly render Nav buttons

--- react/components/Nav.jsx
+++ react/components/Nav.jsx
@@ -1,17 +1,37 @@
 var React = require('react');
-var {Link, IndexLink} = require('react-router');
+var {Link, IndexLink, hashHistory} = require('react-router');
+var auth = require('./../api/authentication.jsx');
 
-var Nav = () => {
-  return (
-    <div className="header">
-      <h1>TODO LIST</h1>
+var Nav = React.createClass({
+  onSubmitLogout (e) {
+    e.preventDefault();
+    auth.logout();
+    hashHistory.push('/');
+  },
+  notLoggedIn () {
+    return (
       <ul className="nav">
         <li><Link to="/signup">Create Account</Link></li>
         <li><IndexLink to="/" href="#signin">Sign In</IndexLink></li>
       </ul>
-      <div className="clear"></div>
-    </div>
-  );
-}
+    );
+  },
+  loggedIn () {
+    return (
+      <ul className="nav">
+        <li><button id="logoutButton" onClick={this.onSubmitLogout}>Sign Out</button></li>
+      </ul>
+    );
+  },
+  render () {
+    return (
+      <div className="header">
+        <h1>TODO LIST</h1>
+        {auth.isAuthenticated() ? this.loggedIn() : this.notLoggedIn()}
+        <div className="clear"></div>
+      </div>
+    );
+  }
+});
 
 module.exports = Nav;
