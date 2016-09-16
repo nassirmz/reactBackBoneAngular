7c9715f5a978e48f7a8ecd1f490e7d5b1b7771cf
nassir <nassirmz@gmail.com>
2016-09-15 22:42:31 -0700

    Add error handler

--- server/utils/routes.js
+++ server/utils/routes.js
@@ -2,17 +2,24 @@ var helpers = require('./helper');
 
 module.exports = function (app) {
 
+  //routing for todos
   app.route('/todos')
     .get(helpers.requireAuthentication, helpers.getAllTodos)
     .post(helpers.requireAuthentication, helpers.createTodo);
+
+  //routing for users
   app.route('/users')
     .post(helpers.createUser);
+
+  //routing for todo
   app.route('/todos/:id')
     .put(helpers.requireAuthentication, helpers.updateTodo)
     .get(helpers.requireAuthentication, helpers.getTodo)
     .delete(helpers.requireAuthentication, helpers.deleteTodo);
+
+  //routing for login a user
   app.route('/users/login')
     .post(helpers.loginUser);
-  app.route('/users/logout')
-    .delete(helpers.requireAuthentication, helpers.logoutUser);
+
+  app.use(helpers.error);
 };
